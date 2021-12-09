ff592c99ed8d75ea2bb953a38496431902347545
Patryk Walendziuk <walenda1997@gmail.com>
2021-12-09 18:37:37 +0100

    create board with colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+const width = 8
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 
-function App() {
+const App = () => {
+  const [currentColorArrangement,setCurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangment = []
+
+    for (let i = 0; i< width * width; i++){
+      const randomNumberFrom0to5 = Math.floor(Math.random()*candyColors.length)
+      const randomColor = candyColors[randomNumberFrom0to5]
+      randomColorArrangment.push(randomColor)
+    }
+  
+    setCurrentColorArrangement(randomColorArrangment)
+  }
+
+  useEffect(() => {
+    createBoard()
+  },[])
+
+  console.log(currentColorArrangement)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
