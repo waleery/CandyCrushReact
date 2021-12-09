8ae46aa7bdb6903c9acc6a03333098848d05bde9
Patryk Walendziuk <walenda1997@gmail.com>
2021-12-09 19:14:03 +0100

    checking columns of 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ const App = () => {
 
   const checkForColumnOfThree = () =>{
     for(let i=0; i<47 ; i++){
-      
+      const columnOfThree = [i, i+width, i+width*2]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
     }
   }
   
@@ -29,6 +34,14 @@ const App = () => {
   useEffect(() => {
     createBoard()
   },[])
+
+  useEffect(() => {
+   const timer  = setInterval(() =>{
+    checkForColumnOfThree()
+    setCurrentColorArrangement([...currentColorArrangement])
+    },100)
+    return () => clearInterval(timer)
+  }, [checkForColumnOfThree, currentColorArrangement])
 
   console.log(currentColorArrangement)
 
