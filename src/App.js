75ae045bb41057dd4138984d11cb74a1f27406d3
Patryk Walendziuk <walenda1997@gmail.com>
2021-12-09 21:57:38 +0100

    checkForColumnOfFour

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 const App = () => {
   const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfFour = () =>{
+    for(let i=0; i<39 ; i++){
+      const columnOfFour = [i, i+width, i+width*2, i+width*3]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const checkForColumnOfThree = () =>{
     for(let i=0; i<47 ; i++){
       const columnOfThree = [i, i+width, i+width*2]
@@ -37,14 +48,16 @@ const App = () => {
 
   useEffect(() => {
    const timer  = setInterval(() =>{
+    checkForColumnOfFour()
     checkForColumnOfThree()
     setCurrentColorArrangement([...currentColorArrangement])
     },100)
     return () => clearInterval(timer)
-  }, [checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
 
   console.log(currentColorArrangement)
 
+  
   return (
     <div className="app">
       <div className="game">
